2348c6c54c76551b2588a636428b60dc96fbfa7a
Internet1235 <135099493+Internet1235@users.noreply.github.com>
2025-01-07 12:33:06 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,15 +3,15 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-rm -rf feeds/luci/themes/luci-theme-argon
+## rm -rf feeds/luci/themes/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
-git clone https://github.com/Internet1235/openwrt-iptvhelper.git package/iptvhelper
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+## git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+## sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
+## git clone https://github.com/Internet1235/openwrt-iptvhelper.git package/iptvhelper
+## git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+## git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+## git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 #echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
